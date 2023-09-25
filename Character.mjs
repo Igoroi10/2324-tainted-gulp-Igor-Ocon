7e8c40e76a3313a81def86635b414322408cfa10
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2023-09-25 12:00:29 +0200

    [NEW] drinkEmAll detects wich type of potion it is and changes attributes

--- Character.mjs
+++ Character.mjs
@@ -12,4 +12,87 @@ export default class Character{
         const fullName = `${playerData.name} the ${playerData.class}`;
         return new Character (fullName, playerData.health, playerData.magick, playerData.stamina, potions.potions)
     }
+
+    drinkEmAll(){
+
+
+        let stillInGame = true;
+
+        this.potions.forEach(currentPotion => {
+
+            if(stillInGame){
+                if(checkIfPotion(currentPotion)){
+                    if(currentPotion.name.includes("Health")){
+                        this.health += currentPotion.value;
+                        console.log(`${this.fullName} drinks ${currentPotion.name} and gains ${currentPotion.value} points of health.`);
+                    }
+                
+                    else if(currentPotion.name.includes("Stamina")){
+                        this.stamina += currentPotion.value;
+                        console.log(`${this.fullName} drinks ${currentPotion.name} and gains ${currentPotion.value} points of stamina.`);
+                    
+                    }
+
+                    else if(currentPotion.name.includes("Magick")){
+                        this.magick += currentPotion.value;
+                        console.log(`${this.fullName} drinks ${currentPotion.name} and gains ${currentPotion.value} points of magick.`);
+                    
+                    }
+
+                    else if(currentPotion.name.includes("Failed")){
+                        console.log(`Failed Potion. ${this.fullName} cannot drink.`);
+                    
+                    }
+
+                    else if(currentPotion.name.includes("Sanity")){
+                        this.health += currentPotion.value;
+                        this.magick += currentPotion.value;
+                        this.stamina += currentPotion.value;
+                        console.log(`${this.fullName} drinks ${currentPotion.name} and gains ${currentPotion.value} points of each attribute.`);
+                    
+                    }
+
+                    else{
+                        this.health++
+                        this.magick++
+                        this.stamina++
+                        console.log(`${this.fullName} drinks ${currentPotion.name} and gains one point of each attribute.`);
+                    }
+                }
+
+                else{
+
+                    if(currentPotion.name.includes("Health")){
+                        this.health -= currentPotion.value;
+                        console.log(`${this.fullName} drinks ${currentPotion.name} and loses ${currentPotion.value} points of health.`);
+                    }
+                
+                    else if(currentPotion.name.includes("Stamina")){
+                        this.stamina -= currentPotion.value;
+                        console.log(`${this.fullName} drinks ${currentPotion.name} and loses ${currentPotion.value} points of stamina.`);
+                    
+                    }
+
+                    else if(currentPotion.name.includes("Magick")){
+                        this.magick -= currentPotion.value;
+                        console.log(`${this.fullName} drinks ${currentPotion.name} and loses ${currentPotion.value} points of magicka.`);
+                    
+                    }
+                }
+
+
+                console.log(`Health:        ${this.health}`);
+                console.log(`Magick:        ${this.magick}`);
+                console.log(`Stamina:       ${this.stamina}`);
+                console.log("-----------------------------------------");
+
+            }
+
+            
+        });
+    }
+}
+
+function checkIfPotion(potion){
+    return potion.name.includes("Potion") || potion.name.includes("potion");
 }
