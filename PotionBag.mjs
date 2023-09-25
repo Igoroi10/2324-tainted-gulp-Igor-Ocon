df116238cd9af4d355848ffaffe174ca61496dd5
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2023-09-25 08:54:44 +0200

    [NEW] PotionBag file as factory function of potions

--- PotionBag.mjs
+++ PotionBag.mjs
@@ -7,6 +7,25 @@ class PotionBag {
     }
 
     createPotions(ingredients, cauldron){
-        
+        const usedIngredients = [];
+        const potionArray = ingredients.forEach(element => {
+
+            ingredients.forEach(el => {
+                const newPotion = cauldron.createPotion(element, el);
+
+                if(!potionAlreadyCreated(el, usedIngredients))
+                    potionArray.push(newPotion);
+
+            })
+
+            usedIngredients.push(element);
+        });
+
+        console.log(potionArray);
     }
+
+
+potionAlreadyCreated(ingredient, array){
+    return array.some(arrayPotion => ingredient.name === arrayPotion.name );
+}
 }
