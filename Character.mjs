a8354965c72fd37387014de91ef816b4aa74d4ff
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2023-09-25 10:14:47 +0200

    [FIX] fixed constructor of Character

--- Character.mjs
+++ Character.mjs
@@ -1,14 +1,15 @@
 
 export default class Character{
     constructor(fullName, health, magick, stamina, potions){
-        fullName,
-        health,
-        magick,
-        stamina,
-        potions
+        this.fullName       = fullName;
+        this.health         = health;
+        this.magick         = magick;
+        this.staminta       = stamina;
+        this.potions        = potions;
     }
 
     from(playerData, potions){
+        console.log(playerData.name)
         const fullName = `${playerData.name} the ${playerData.class}`;
         return new Character (fullName, playerData.health, playerData.magick, playerData.stamina, potions)
     }
